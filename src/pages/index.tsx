9a96d7ab52556db7a66b019e0e6bea7df9a90ddf
Jiho Bok <bok.jiho@gmail.com>
2025-12-13 21:19:45 +0000

    chore: update app metadata
    title & description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,24 +8,20 @@ const inter = Inter({ subsets: ["latin"] });
 const AppWithoutSSR = dynamic(() => import("@/App"), { ssr: false });
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Phaser Nextjs Template</title>
-                <meta
-                    name="description"
-                    content="A Phaser 3 Next.js project template that demonstrates Next.js with React communication and uses Vite for bundling."
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <link rel="icon" href="/favicon.png" />
-            </Head>
-            <main className={`${styles.main} ${inter.className}`}>
-                <AppWithoutSSR />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Rudolph's Present Game</title>
+        <meta
+          name="description"
+          content="Play Rudolph's Presents Game to find out what you're friends & families want for Christmas"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <AppWithoutSSR />
+      </main>
+    </>
+  );
 }
-
