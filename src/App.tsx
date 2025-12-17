f669748f0b5273f425bbc4c3cf6643169d097687
Jiho Bok <bok.jiho@gmail.com>
2025-12-17 21:57:30 +0000

    refactor: change rudolph game into class based scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { IRefPhaserGame, PhaserGame } from "./PhaserGame";
 import { useRouter } from "next/router";
+import RudolphGame from "./game/scenes/RudolphGame";
 
 function App() {
   // The sprite can only be moved in the MainMenu Scene
@@ -20,11 +21,33 @@ function App() {
 
   // Event emitted from the PhaserGame component
   const currentScene = (scene: Phaser.Scene) => {
+    /** 이 부분 key 업뎃 해야 되나? 지워도 되나 */
     setCanMoveSprite(scene.scene.key !== "MainMenu");
+  };
+
+  const moveSprite = () => {
+    console.log("clicked11111111");
+
+    if (phaserRef.current) {
+      const scene = phaserRef.current.scene as RudolphGame;
+      console.log("22222222", scene);
+
+      // if (scene && scene.scene.key === "MainMenu") {
+      if (scene && scene.scene.key === "RudolphGame") {
+        console.log("333333333");
+        // Get the update logo position
+        // scene.moveLogo(({ x, y }) => {
+        //   setSpritePosition({ x, y });
+        // });
+
+        scene.spawnStar(50, ["beer"]);
+      }
+    }
   };
 
   return (
     <div id="app">
+      <button onClick={moveSprite}>spawn item</button>
       <PhaserGame
         ref={phaserRef}
         currentActiveScene={currentScene}
---
+++ src/game/scenes/RudolphGame.js
@@ -0,0 +1,313 @@
+import Phaser from "phaser";
+import { tempItems } from "../items.ts";
+
+const GAME_PLAY_TIME = 20000;
+const GAME_WIDTH = 365;
+const GAME_HEIGHT = 500;
+
+export default class RudolphGame extends Phaser.Scene {
+  constructor() {
+    super("RudolphGame");
+
+    // game objects & images
+    this.player;
+    this.stars;
+    this.bombs;
+    this.platforms;
+    this.background;
+
+    // Inputs
+    this.leftKey;
+    this.rightKey;
+
+    this.score = 0;
+    this.scoreText;
+
+    this.remainingTimeText;
+    this.gameTimer;
+    this.gameOver = false;
+    this.starSpawnId = null;
+    this.bombSpawnId = null;
+
+    this.likedItems = new Set();
+    this.dislikedItems = new Set();
+    this.likedItemsSize = 0;
+    this.dislikedItemsSize = 0;
+  }
+
+  generateRandomInteger(min, max) {
+    return min + Math.floor(Math.random() * max);
+  }
+
+  collectStar(player, star) {
+    star.disableBody(true, true);
+
+    player.preFX.addGlow(0x00b90c); // green
+    setTimeout(() => player.preFX.clear(), 300);
+
+    this.score += 10;
+    this.scoreText.setText("Score: " + this.score);
+
+    this.likedItems.add(star.name);
+  }
+
+  itemHitsPlatform(platforms, item) {
+    item.disableBody(true, true);
+  }
+
+  bombHitsPlayer(player, bomb) {
+    this.score -= 5;
+    this.scoreText.setText("Score: " + this.score);
+    bomb.disableBody(true, true);
+    player.preFX.addGlow(0xff0000); // red
+    setTimeout(() => player.preFX.clear(), 300);
+
+    this.dislikedItems.add(bomb.name);
+  }
+
+  spawnStar(x, items = []) {
+    // gameObject.setRandomPosition
+    const itemList = items.length > 0 ? items : ["star", "ring", "cash"];
+    const item = itemList[this.generateRandomInteger(0, itemList.length)];
+    let star = this.stars.create(x, 16, item);
+    star.name = item;
+    star.setDisplaySize(25, 25);
+    star.setCollideWorldBounds(true);
+
+    const DROP_SPEED = 50;
+    star.setVelocity(0, DROP_SPEED);
+  }
+
+  spwanBomb(x) {
+    let bomb = this.bombs.create(x, 16, "bomb");
+    // bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.name = "bomb";
+
+    const DROP_SPEED = 50; // bigger the value, faster drop
+    bomb.setVelocity(0, DROP_SPEED);
+  }
+
+  handleGameOver() {
+    this.player.stop();
+    this.gameTimer.destroy();
+    this.remainingTimeText.setText("Remaining time: 0s");
+
+    this.gameOver = true;
+  }
+
+  startTimer() {
+    this.gameTimer = this.time.addEvent({
+      delay: GAME_PLAY_TIME, // ms
+      callback: () => this.handleGameOver(),
+      //args: [],
+      // callbackScope: thisArg,
+    });
+  }
+
+  preload() {
+    this.load.image("background", "assets/christmas-bg.jpg");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("snow-ground", "assets/snow-platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("rudolph", "assets/rudolph-v2.png", {
+      frameWidth: 64,
+      frameHeight: 92,
+    });
+
+    // items
+    Object.entries(tempItems).forEach(([key, data]) => {
+      // console.log(key, data, "<<<<");
+
+      this.load.image(key, data.path);
+    });
+    //   this.load.image("ring", "assets/items/ring.svg");
+    //   this.load.image("cash", "assets/items/cash.svg");
+    //   this.load.image("bike", "assets/items/bike.svg");
+    //   this.load.image("beer", "assets/items/beer.svg");
+    //   this.load.image("cat", "assets/items/cat.svg");
+  }
+
+  create() {
+    this.background = this.make.image({
+      x: 182,
+      y: GAME_HEIGHT / 2 - 10,
+      key: "background",
+      scale: { x: 1.1, y: 1.1 },
+    });
+
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms
+      .create(GAME_WIDTH / 2, GAME_HEIGHT, "snow-ground")
+      .refreshBody();
+
+    this.player = this.physics.add.sprite(
+      GAME_WIDTH / 2,
+      GAME_HEIGHT - 80,
+      "rudolph"
+    );
+    this.player.setScale(0.8).refreshBody();
+    this.player.setCollideWorldBounds(true);
+
+    /** rudolph anims */
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("rudolph", {
+        start: 0,
+        end: 1,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "rudolph", frame: 2 }],
+      frameRate: 20,
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("rudolph", {
+        start: 3,
+        end: 4,
+      }),
+      frameRate: 10,
+      repeat: -1,
+    });
+
+    this.leftKey = this.input.keyboard.addKey("LEFT"); // Get key object
+    this.rightKey = this.input.keyboard.addKey("RIGHT"); // Get key object
+
+    this.stars = this.physics.add.group();
+    this.starSpawnId = setInterval(
+      () => this.spawnStar(this.generateRandomInteger(10, GAME_WIDTH)),
+      800
+    );
+
+    this.bombs = this.physics.add.group();
+    this.bombSpawnId = setInterval(
+      () => this.spwanBomb(this.generateRandomInteger(10, GAME_WIDTH)),
+      1000
+    );
+
+    this.scoreText = this.add.text(16, 16, "score: 0", {
+      fontSize: "20px",
+      fill: "#000",
+      backgroundColor: "rgba(255, 255, 255, 0.5)",
+    });
+
+    this.remainingTimeText = this.add.text(
+      16,
+      38,
+      `Remaining Time: ${GAME_PLAY_TIME / 1000 + 1}s`,
+      {
+        fontSize: "20px",
+        fill: "#000",
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
+      }
+    );
+
+    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(
+      this.platforms,
+      this.stars,
+      this.itemHitsPlatform,
+      null,
+      this
+    );
+    this.physics.add.collider(
+      this.platforms,
+      this.bombs,
+      this.itemHitsPlatform,
+      null,
+      this
+    );
+    this.physics.add.collider(
+      this.player,
+      this.bombs,
+      this.bombHitsPlayer,
+      null,
+      this
+    );
+
+    this.physics.add.overlap(
+      this.player,
+      this.stars,
+      this.collectStar,
+      null,
+      this
+    );
+
+    this.startTimer();
+  }
+
+  update() {
+    if (this.gameOver) {
+      clearInterval(this.starSpawnId);
+      clearInterval(this.bombSpawnId);
+
+      this.game.events.emit("game-over", { score: this.score });
+      this.game.pause();
+
+      return;
+      // game over scene 으로 전환?
+    }
+
+    //   let viewport = this.scene.scale.getViewPort();
+    //   var parentSize = this.scene.scale.parentSize;
+
+    //   console.log(this.scene.scene.scale.getViewPort(), "<<< sizes");
+
+    const { width, height } = this.scale.getViewPort();
+    if (width < GAME_WIDTH) {
+      this.scale.setGameSize(width, width * (GAME_HEIGHT / GAME_WIDTH));
+
+      this.background.setPosition(width / 2, height / 2);
+      this.background.setDisplaySize(width, height);
+      // console.log(platforms);
+      this.platforms.setXY(width / 2, height);
+      // platforms.refreshBody();
+
+      this.player.setY(height - 50);
+    }
+
+    if (
+      this.leftKey.isDown ||
+      (this.input.activePointer.isDown &&
+        this.input.activePointer.x < GAME_WIDTH / 2)
+    ) {
+      this.player.setVelocityX(-400);
+      this.player.anims.play("left", true);
+    } else if (
+      this.rightKey.isDown ||
+      (this.input.activePointer.isDown &&
+        this.input.activePointer.x >= GAME_WIDTH / 2)
+    ) {
+      this.player.setVelocityX(400);
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+      this.player.anims.play("turn");
+    }
+
+    let remainingTime = Math.floor(this.gameTimer.getRemainingSeconds());
+
+    if (remainingTime >= 0) {
+      this.remainingTimeText.setText(`Remaining time: ${remainingTime + 1}s`);
+    }
+
+    if (this.likedItemsSize !== this.likedItems.size) {
+      this.likedItemsSize = this.likedItems.size;
+      const items = Array.from(this.likedItems);
+      this.game.events.emit("update-itemList", items);
+    }
+
+    if (this.dislikedItemsSize !== this.dislikedItems.size) {
+      this.dislikedItemsSize = this.dislikedItems.size;
+      const items = Array.from(this.dislikedItems);
+      this.game.events.emit("update-dislikes", items);
+    }
+  }
+}
