4c9c9ed9afd60797d3392293780cfbdaee014041
Jiho Bok <bok.jiho@gmail.com>
2025-12-19 23:11:44 +0000

    feat: update game name
    + metadata

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -4,26 +4,22 @@ function MainPage() {
   return (
     <section className="flex flex-col justify-center gap-5 w-9/10 sm:max-w-125 h-dvh overflow-y-scroll py-10 mx-auto text-center">
       <h1 className="text-2xl font-extrabold text-green-600">
-        Rudolph's Present Game 𐂂𐂂
+        Secret Rudolph 𐂂𐂂
       </h1>
       <p>Hohoho it's holiday seasons! </p>
       <p className="text-3xl">🎄🎅☃️</p>
       <p>
-        {" "}
         What would you like for Christmas? Make a customised game with your
         wishlist and share with your friends.
       </p>
       <p>Also, find out what your loved ones would like to get for Christmas</p>
-      {/* <p>
-        You will select items you would like and dislike for Christmas presents.
-        Your friends will then try to guess your wishlist by playing the game.
-      </p> */}
+
       <p>Let's get started!</p>
 
       <div className="sm:mt-5 flex flex-col w-4/5 sm:w-full sm:flex-row gap-4 justify-center mx-auto">
         <Link
           href="/new-game"
-          className="sm:flex-1 p-5 rounded-2xl font-semibold bg-red-700 hover:bg-red-800 hover:cursor-pointer"
+          className="sm:flex-1 p-5 rounded-2xl font-semibold bg-white hover:bg-green-200 border-2 text-green-800 border-green-600 hover:cursor-pointer"
         >
           <div className="text-xl">Create new game</div>
           <div className="font-medium mt-2 text-sm">
